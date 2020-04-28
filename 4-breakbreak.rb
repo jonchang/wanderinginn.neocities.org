0c6528e1c07204b074925a52c1729502eb3b4f44
Jonathan Chang <me@jonathanchang.org>
2020-04-29 03:32:54 +1000

    Emit diffstats instead

--- 4-breakbreak.rb
+++ 4-breakbreak.rb
@@ -6,38 +6,47 @@ require 'ruby-progressbar'
 require 'diffy'
 
 def slug_from_fn(fn)
-    fn =~ %r{([\w-]+)/(\d{14})}
-    slug = Regexp.last_match(1)
-    datestamp = Regexp.last_match(2)
-    return slug, datestamp
+  fn =~ %r{([\w-]+)/(\d{14})}
+  slug = Regexp.last_match(1)
+  datestamp = Regexp.last_match(2)
+  return slug, datestamp
 end
 
 # https://news.ycombinator.com/item?id=13997533
 def breakerbreaker(text)
-    text.gsub! %r{([.,?!:;)'"–…]) }, "\\1 \n"
-    text.gsub "…", "…\n"
+  text.gsub! %r{([.,?!:;)'"–…]) }, "\\1 \n"
+  text.gsub "…", "…\n"
 end
 
 FileUtils.mkdir_p "_site/diffs"
 open("_site/diffs/diff.css", "w") do |f|
-    f.puts Diffy::CSS
+  f.puts Diffy::CSS
 end
 
 texts = Dir['texts/*/'].sort
 
 Parallel.each(texts, in_processes: 4, progress: 'Diffing') do |dir|
-    texts = Dir["#{dir}*"].sort
-    next if texts.length < 2
-    aa = texts.first
-    bb = texts.last
-    slug, date_a = slug_from_fn aa
-    slug, date_b = slug_from_fn bb
-    broke_a = breakerbreaker open(aa).read
-    broke_b = breakerbreaker open(bb).read
-    diff_html = Diffy::Diff.new(broke_a, broke_b, :context => 2).to_s(:html)
-    next if diff_html =~ %r{<div class="diff"></div>}
-    open("_site/diffs/#{slug}.html", "w") do |f|
-        f.puts '<link rel="stylesheet" href="diff.css"/>'
-        f.puts diff_html
-    end
+  texts = Dir["#{dir}*"].sort
+  next if texts.length < 2
+  aa = texts.first
+  bb = texts.last
+  slug, date_a = slug_from_fn aa
+  slug, date_b = slug_from_fn bb
+  broke_a = breakerbreaker open(aa).read
+  broke_b = breakerbreaker open(bb).read
+  diff_html = Diffy::Diff.new(broke_a, broke_b, :context => 2).to_s(:html)
+  next if diff_html =~ %r{<div class="diff"></div>}
+  open("_site/diffs/#{slug}.html", "w") do |f|
+    f.puts <<~EOHTML
+    <!doctype html>
+    <html>
+    <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="diff.css"/>
+    </head>
+    <body>
+    #{diff_html}
+    </body></html>
+    EOHTML
+  end
 end
