b87a49dc12d81f0d17df1e627dd5633cc56e2977
Jonathan Chang <jchang641@gmail.com>
2020-05-20 01:44:53 +1000

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,34 @@
-#[macro_use] extern crate lazy_static;
+#[macro_use]
+extern crate lazy_static;
+use dissolve::strip_html_tags;
 use exitfailure::ExitFailure;
 use glob::glob;
+use indicatif::ParallelProgressIterator;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 use readability::extractor;
 use regex::Regex;
-use std::fs::{File, create_dir_all};
+use std::fs::{create_dir_all, File};
 use std::io::Write;
-use std::path::{PathBuf, Path};
+use std::path::{Path, PathBuf};
 use url::Url;
-use indicatif::ParallelProgressIterator;
-use rayon::iter::{ParallelIterator, IntoParallelRefIterator};
-use dissolve::strip_html_tags;
 
 fn fix_text(text: String) -> String {
     lazy_static! {
-        static ref RE1: Regex = Regex::new(r"Previous Chapter|Next Chapter|</?span>|</p>|</?em>").unwrap();
+        static ref RE1: Regex =
+            Regex::new(r"Previous Chapter|Next Chapter|</?span>|</p>|</?em>").unwrap();
         static ref RE2: Regex = Regex::new(r"\n{3,}").unwrap();
     }
-    strip_html_tags(&RE2.replace_all(&RE1.replace_all(&text, "")
-                                     .replace("<p>", "\n\n")
-                                     .replace("\u{00A0}", " "), "\n\n")).join("").trim().to_string()
+    strip_html_tags(
+        &RE2.replace_all(
+            &RE1.replace_all(&text, "")
+                .replace("<p>", "\n\n")
+                .replace("\u{00A0}", " "),
+            "\n\n",
+        ),
+    )
+    .join("")
+    .trim()
+    .to_string()
 }
 
 fn get_datestamp(file: &str) -> &str {
@@ -29,13 +39,24 @@ fn get_datestamp(file: &str) -> &str {
 }
 
 fn get_slug(file: &PathBuf) -> &str {
-    file.parent().expect("No parent found!").components().last().expect("No last component!").as_os_str().to_str().expect("Couldn't convert to str")
+    file.parent()
+        .expect("No parent found!")
+        .components()
+        .last()
+        .expect("No last component!")
+        .as_os_str()
+        .to_str()
+        .expect("Couldn't convert to str")
 }
 
 fn do_extract<P: AsRef<Path>>(file: P) -> String {
     let mut fh = File::open(&file).expect("Failed to open file");
-    let mut url =  Url::parse("http://wanderinginn.com").expect("Parsing error");
-    fix_text(extractor::extract(&mut fh, &mut url).expect("Failed to extract text").content)
+    let mut url = Url::parse("http://wanderinginn.com").expect("Parsing error");
+    fix_text(
+        extractor::extract(&mut fh, &mut url)
+            .expect("Failed to extract text")
+            .content,
+    )
 }
 
 fn write_to_path(text: &str, slug: &str, datestamp: &str) {
@@ -45,7 +66,6 @@ fn write_to_path(text: &str, slug: &str, datestamp: &str) {
     let mut outfile = File::create(path).expect("Create file failed");
     outfile.write_all(text.as_bytes()).expect("Write failed");
 }
-
 
 fn main() -> Result<(), ExitFailure> {
     let files: Vec<_> = glob("/Users/jonchang/TWI/websites/**/index.html")?
