9917a301a9211650a7f671817f87b2e5ea995b16
Jonathan Chang <jchang641@gmail.com>
2020-06-24 17:06:24 +1000

    Don't redownload entire site

--- 2-download-site.sh
+++ 2-download-site.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-parallel --csv -j4 --delay 10 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps :::: data.csv
+parallel --csv -j4 --delay 10 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps ::: $(head -n21 data.csv | tail -n20)
