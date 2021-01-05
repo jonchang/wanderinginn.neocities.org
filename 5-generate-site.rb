d76170650ef46b7d13ac59cdde75cf15e6882fcb
Jonathan Chang <me@jonathanchang.org>
2021-01-05 13:59:47 +1100

    Support multiple erb files

--- 5-generate-site.rb
+++ 5-generate-site.rb
@@ -5,16 +5,7 @@ require 'erb'
 require 'fileutils'
 require 'ostruct'
 
-data = CSV.read("data.csv", headers: true, header_converters: :symbol)
-
-data = data.sort { |a, b| a[0] <=> b[0] }
-
-erb_file = "index.html.erb"
-html_file = "_site/" + File.basename(erb_file, ".erb") #=>"index.html"
-
-erb_str = File.read(erb_file)
-
-variables = OpenStruct.new
+ERB_FILES = %w[index.html.erb]
 
 def generate_chapter_link(row)
     %Q|<a name="chapter-#{row[:slug]}" href="#{row[:url]}">#{row[:title]}</a>|
@@ -59,13 +50,18 @@ def generate_html_table(dd)
   end.join
 end
 
+data = CSV.read("data.csv", headers: true, header_converters: :symbol)
+data = data.sort { |a, b| a[0] <=> b[0] }
+
+variables = OpenStruct.new
 variables[:lastmod] = DateTime.now.strftime("%B %-d, %Y")
 variables[:html_table] = generate_html_table(data)
 
-res = ERB.new(erb_str, trim_mode: ">").result(variables.instance_eval { binding })
-
-File.open(html_file, 'w') do |f|
-  f.write(res)
+ERB_FILES.each do |erb|
+  html_file = "_site/" + File.basename(erb, ".erb") #=>"index.html"
+  erb_str = File.read(erb)
+  res = ERB.new(erb_str, trim_mode: ">").result(variables.instance_eval { binding })
+  File.open(html_file, 'w') { |f| f.write(res) }
 end
 
 FileUtils.cp "header.png", "_site/wise_papa_sloth_corrupted_by_his_own_power_can_no_leader_go_untainted.png"
