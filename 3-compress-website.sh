e06d67eed4f3bbfde88fcc84a8fd4b581680190c
Jonathan Chang <me@jonathanchang.org>
2021-04-02 14:59:43 -0700

    Move compress to stage 3 and force write

---
+++ 3-compress-website.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+COMPRESSION_LEVEL=3
+OUTPUT_FILE=all_text.tar.xz
+
+find websites -not -path '*/\.*' -type f | rev | sort | rev | tar cf - -T -  | xz -${COMPRESSION_LEVEL} -v --force -T0 > "$OUTPUT_FILE"
