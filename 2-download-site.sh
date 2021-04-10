59a95dde006668c198f2336adf16f0e2c3623ed8
Jonathan Chang <me@jonathanchang.org>
2021-04-10 16:04:28 -0700

    Avoid `head | tail | parallel` with proper args

--- 2-download-site.sh
+++ 2-download-site.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-head -n21 data.csv | tail -n20 | parallel --delay 10 --csv -j4 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
+head -n21 data.csv | parallel --delay 10 --csv --skip-first-line -j4 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
