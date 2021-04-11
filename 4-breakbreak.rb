369b0529e6122fdd4d90cef097dace5b32cdfb1f
Jonathan Chang <me@jonathanchang.org>
2021-04-10 20:20:55 -0700

    Space out diffs more

--- 4-breakbreak.rb
+++ 4-breakbreak.rb
@@ -15,6 +15,27 @@ def slug_from_fn(filename)
   [slug, datestamp]
 end
 
+# https://news.ycombinator.com/item?id=13997533
+def breakerbreaker(text)
+  text.gsub! "\u00A0", ' '
+  text.gsub!(/([.,?!:;)'"–…]) /, "\\1 \n")
+  text.gsub '…', "…\n"
+end
+
+# Create a bit of spacing between each "chunk"
+def tweak_diffy_html(html)
+  html.lines.map do |line|
+    case line
+    when /class="diff-block-info"/
+      "</ul>\n<ul>\n"
+    when /class="diff-comment"/ then nil
+    when %r{<span></span>} then nil
+    else
+      line
+    end
+  end.compact.join
+end
+
 data = CSV.read('data.csv', headers: true, header_converters: :symbol)
 
 df = {}
@@ -27,20 +48,13 @@ data.each do |row|
   }
 end
 
-# https://news.ycombinator.com/item?id=13997533
-def breakerbreaker(text)
-  text.gsub! "\u00A0", ' '
-  text.gsub!(/([.,?!:;)'"–…]) /, "\\1 \n")
-  text.gsub '…', "…\n"
-end
-
 FileUtils.mkdir_p '_site/diffs'
 FileUtils.mkdir_p '_site/texts'
 
 open('_site/diffs/diff.css', 'w') do |f|
   f.puts <<~EOCSS
     .diff{overflow:auto;}
-    .diff ul{background:#fff;overflow:auto;font-size:13px;list-style:none;margin:0;padding:0;display:table;width:100%;}
+    .diff ul{background:#fff;overflow:auto;font-size:13px;list-style:none;margin:2.5ex 0;padding:0;display:table;width:100%;}
     .diff del, .diff ins{display:block;text-decoration:none;}
     .diff li{padding:0; display:table-row;margin: 0;height:1em;}
     .diff li.ins{background:#dfd; color:#080}
@@ -62,17 +76,20 @@ Parallel.each(texts, in_processes: 4, progress: 'Diffing') do |dir|
   texts = Dir["#{dir}*"].sort
   aa = texts.first
   bb = texts.last
-  _, date_a = slug_from_fn aa
+  slug_a, date_a = slug_from_fn aa
   slug, date_b = slug_from_fn bb
-  File.open("_site/texts/#{slug}.txt", 'w') do |f|
-    f.write(File.open(bb).read)
-  end
+  raise unless slug_a == slug
+
+  File.open("_site/texts/#{slug}.txt", 'w') { |f| f.write(File.open(bb).read) }
   next if texts.length < 2
 
   broke_a = breakerbreaker File.open(aa).read
   broke_b = breakerbreaker File.open(bb).read
-  diff_html = Diffy::Diff.new(broke_a, broke_b, context: 2).to_s(:html)
+  diff_html = Diffy::Diff.new(broke_a, broke_b, context: 1, include_diff_info: true).to_s(:html)
+
   next if diff_html =~ %r{<div class="diff"></div>}
+
+  diff_html = tweak_diffy_html(diff_html)
 
   variables = OpenStruct.new
   variables[:diff_html] = diff_html
@@ -81,7 +98,5 @@ Parallel.each(texts, in_processes: 4, progress: 'Diffing') do |dir|
   variables[:back_link] = "/#chapter-#{slug}"
   res = ERB.new(erb_text, trim_mode: '>').result(variables.instance_eval { binding })
 
-  File.open("_site/diffs/#{slug}.html", 'w') do |f|
-    f.write(res)
-  end
+  File.open("_site/diffs/#{slug}.html", 'w') { |f| f.write(res) }
 end
