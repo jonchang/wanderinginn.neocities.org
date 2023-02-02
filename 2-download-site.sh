4874a0f62fcce47eb81258ca2eeb274e14209258
Jonathan Chang <me@jonathanchang.org>
2023-02-01 19:23:56 -0800

    Decrease parallelism

--- 2-download-site.sh
+++ 2-download-site.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-head -n21 data.csv | parallel --delay 10 --csv --skip-first-line -j4 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
+head -n21 data.csv | parallel --delay 10 --csv --skip-first-line -j2 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
