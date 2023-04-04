bcbfd0eb68e9e7b9a77d12b6dd21d4b2fd23c03a
Jonathan Chang <me@jonathanchang.org>
2023-04-03 19:02:16 -0700

    Update workflow to use `make` commands

--- 2-download-site.sh
+++ 2-download-site.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-head -n21 data.csv | parallel --delay 10 --csv --skip-first-line -j2 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
+head -n100 data.csv | parallel --delay 10 --retries 3 --csv --skip-first-line -j2 bundle exec wayback_machine_downloader {1} --exact-url --all-timestamps
